15086eece9b1c1c3b0363d9c127f1f571bb7be6f
Daniel Meszaros <easimer@gmail.com>
2018-02-11 00:02:38 +0000

    Add model loader funcs to renderer, fix smd_parser symlink

--- game/renderer.cpp
+++ game/renderer.cpp
@@ -5,6 +5,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 #include <GL/glu.h>
+#include <assert_opengl.h>
+
+#include "qc_parser.h"
+#include "smd_parser.h"
 
 void renderer::open_window(const char * szTitle, int nWidth, int nHeight, bool bFullscreen)
 {
@@ -30,29 +34,118 @@ void renderer::close_window()
 void renderer::render()
 {
 	RESTRICT_THREAD_RENDERING;
+
+	// Draw models
+	while (!m_cmdbuf.IsClosed());
+	drawcmd_t* pCommands;
+	size_t nCommands;
+	m_cmdbuf.BeginRead(&pCommands, &nCommands);
+	m_cmdbuf.EndRead();
+
 	SDL_GL_SwapWindow(m_pWindow);
 }
 
 bool renderer::init_gl()
 {
 	RESTRICT_THREAD_RENDERING;
-	return false;
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	m_pGLContext = SDL_GL_CreateContext(m_pWindow);
+
+	if (!m_pGLContext)
+	{
+		ASSERT_SDL2(0);
+		return false;
+	}
+
+	gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+	PRINT_DBG("GL Vendor: " << glGetString(GL_VENDOR));
+	PRINT_DBG("GL Renderer: " << glGetString(GL_RENDERER));
+	PRINT_DBG("GL Version: " << glGetString(GL_VERSION));
+
+	SDL_GL_SetSwapInterval(1);
+
+	int nWidth, nHeight;
+	SDL_GetWindowSize(m_pWindow, &nWidth, &nHeight);
+
+	glViewport(0, 0, nWidth, nHeight);
+	ASSERT_OPENGL();
+	glClearColor(0.39215686274f, 0.58431372549f, 0.9294117647f, 1.0f);
+	ASSERT_OPENGL();
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	glEnable(GL_MULTISAMPLE);
+
+	return true;
 }
 
 void renderer::shutdown_gl()
 {
 	RESTRICT_THREAD_RENDERING;
+	if (m_pGLContext)
+		SDL_GL_DeleteContext(m_pGLContext);
 }
 
+
+model_id renderer::load_model( const char * szFilename)
+{
+	RESTRICT_THREAD_LOGIC;
+	ldmdl_cmd_t c;
+	strncpy(c.szFilename, szFilename, LDMDL_CMD_MAX_FN);
+	m_ldmdl_cmdbuf.BeginWrite();
+	m_ldmdl_cmdbuf.Write(c);
+	m_ldmdl_cmdbuf.EndWrite();
+	// wait for renderer thread to upload the model
+	while (m_iLoadedModelID == 0);
+	// save model id
+	model_id ret = m_iLoadedModelID;
+	// signal renderer thread to load next model
+	m_iLoadedModelID = 0;
+	return ret;
+}
 void renderer::draw_model(size_t iModelID, vec & vecPosition, float flRotation)
 {
-	RESTRICT_THREAD_RENDERING;
-	while (!m_cmdbuf.IsClosed()) {}
-	
-	drawcmd_t* pCommands;
-	size_t nCommands;
+	RESTRICT_THREAD_LOGIC;
+	drawcmd_t c = { iModelID, vecPosition, flRotation };
+	m_cmdbuf.BeginWrite();
+	m_cmdbuf.Write(c);
+	m_cmdbuf.EndWrite();
+}
 
-	m_cmdbuf.BeginRead(&pCommands, &nCommands);
-
-	m_cmdbuf.EndRead();
+void renderer::model_load_loop()
+{
+	PRINT_DBG("renderer::model_load_loop entered");
+	while (m_bLoading)
+	{
+		while (m_bLoading && (m_ldmdl_cmdbuf.IsEmpty()));
+		if (!m_ldmdl_cmdbuf.IsEmpty())
+		{
+			ldmdl_cmd_t* pCommands;
+			size_t nCommands;
+			m_ldmdl_cmdbuf.BeginRead(&pCommands, &nCommands);
+			while (nCommands--)
+			{
+				mdlc::smd_parser parser(pCommands->szFilename);
+				
+				pCommands++;
+			}
+			m_ldmdl_cmdbuf.EndRead();
+		}
+	}
+	PRINT_DBG("renderer::model_load_loop ended");
 }
--- game/renderer.h
+++ game/renderer.h
@@ -3,13 +3,30 @@
 #include <SDL2/SDL.h>
 #include "cmdbuf.h"
 
+//
+// model draw cmdbuf
+//
+
 CMDBUF_BEGIN_CMD(drawcmd_t)
 	size_t iModelID;
 	vec vecPosition;
 	float flRotation;
 CMDBUF_END_CMD(drawcmd_t)
 
-CMDBUF_DEF(renderer_cmdbuf, drawcmd_t, ENTSYS_MAX_ENTITIES, true, false);
+CMDBUF_DEF(renderer_drawmdl_cmdbuf, drawcmd_t, ENTSYS_MAX_ENTITIES, true, false);
+
+//
+// model load cmdbuf
+//
+
+#define LDMDL_CMD_MAX_FN 64
+typedef size_t model_id;
+
+CMDBUF_BEGIN_CMD(ldmdl_cmd_t)
+	char szFilename[LDMDL_CMD_MAX_FN];
+CMDBUF_END_CMD(ldmdl_cmd_t)
+
+CMDBUF_DEF(renderer_ldmdl_cmdbuf, ldmdl_cmd_t, 512, true, false);
 
 class renderer {
 public:
@@ -19,11 +36,35 @@ public:
 	bool init_gl();
 	void shutdown_gl();
 
+	void begin_load() {
+		RESTRICT_THREAD_LOGIC;
+		if (m_bLoading)
+			return;
+		m_bLoading = true;
+	}
+
+	void end_load() {
+		RESTRICT_THREAD_LOGIC;
+		if (!m_bLoading)
+			return;
+		m_bLoading = false;
+	}
+
+	model_id load_model(const char * szFilename);
 	void draw_model(size_t iModelID, vec& vecPosition, float flRotation);
 
 private:
-	renderer_cmdbuf m_cmdbuf;
+	void model_load_loop();
+
+private:
+	renderer_drawmdl_cmdbuf m_cmdbuf;
+	renderer_ldmdl_cmdbuf m_ldmdl_cmdbuf;
 
 	SDL_Window* m_pWindow;
 	SDL_Renderer* m_pRenderer;
+	SDL_GLContext m_pGLContext;
+
+	bool m_bLoading = false;
+
+	size_t m_iLoadedModelID = 0;
 };
